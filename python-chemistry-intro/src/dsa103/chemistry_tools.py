21102767606c84f77516116df0c0100f5f5711dc
Hugo Peeters <hugo2005peeters@gmail.com>
2025-10-05 11:03:40 +0200

    feat: added a function for calculating molecular weight and a test for this function

--- python-chemistry-intro/src/dsa103/chemistry_tools.py
+++ python-chemistry-intro/src/dsa103/chemistry_tools.py
@@ -10,3 +10,13 @@ def calculate_molecular_formula(smiles: str) -> float:
         raise ValueError("Invalid Smiles string. Could not parse molecule.")
     
     return rdMolDescriptors.CalcMolFormula(mol)
+
+def calculate_molecular_weight(molecule: str) -> float:
+    """Calculates the molecular weight of a molecule given its string. outputs the monoisotopic mass"""
+
+    mol = Chem.MolFromSmiles(molecule)
+
+    if mol is None:
+        raise ValueError("Invalid Smiles string. Could not parse molecule.")
+    
+    return rdMolDescriptors.CalcExactMolWt(mol)
--- python-chemistry-intro/tests/test_chemistry_tools.py
+++ python-chemistry-intro/tests/test_chemistry_tools.py
@@ -1,4 +1,6 @@
-from dsa103.chemistry_tools import calculate_molecular_formula
+from pytest import approx
+
+from dsa103.chemistry_tools import calculate_molecular_formula, calculate_molecular_weight
 
 
 def test_calculate_molecular_formula():
@@ -9,3 +11,11 @@ def test_calculate_molecular_formula():
 
     formula_caffeine = calculate_molecular_formula("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
     assert formula_caffeine == "C8H10N4O2"
+
+def test_calculate_molecular_weight():
+    """Test for calculate_molecular_weight."""
+    weight_water = calculate_molecular_weight("O")
+    assert weight_water == approx(18.0105646863)
+
+    weight_caffeine = calculate_molecular_weight("CN1C=NC2=C1C(=O)N(C(=O)N2C)C")
+    assert weight_caffeine == approx(194.080376)
